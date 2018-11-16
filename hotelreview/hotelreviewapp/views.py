7a5f6818460242bd98225c3305f838d5b09f1d91
Jeong Ook Moon <moonjeongook@gmail.com>
2018-11-16 14:57:33 -0800

    fixed url for django

--- hotelreview/hotelreviewapp/views.py
+++ hotelreview/hotelreviewapp/views.py
@@ -1,21 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
-
-from django.http import HttpResponse
-from django import forms
-
-
-#def index(request):
- #   return HttpResponse("Hello, world. You're at the hotel review.")
-
-
-def reviewform(request):
-    return render(request, './reviewform.html')
-
-def reviewscore(request):
-    if 'positive' in request.GET:
-        message = 'You searched for: %r' % request.GET['positive']
-    if 'negative' in request.GET:
-        message = 'You searched for: %r' % request.GET['negative']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+from django.views.generic import TemplateView
+class HomeView(TemplateView):
+    template_name = 'reviewform.html'
