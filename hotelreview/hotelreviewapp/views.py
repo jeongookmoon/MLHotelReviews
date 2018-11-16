6fdf9603ad3c33d458a7c13307952b081f9c4f97
Ziyun <ziyun@Ziyuns-MacBook-Air.local>
2018-11-16 15:07:43 -0800

    apply rake at view

--- hotelreview/hotelreviewapp/views.py
+++ hotelreview/hotelreviewapp/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from multi_rake import Rake
+
 class HomeView(TemplateView):
     template_name = 'reviewform.html'
+
+rake = Rake()
+print(rake.apply("i like coffee"))
