711f2c9d0676f0a5c2d1d427c73d01b46123bbcf
Ziyun He <ziyunhe@gmail.com>
2018-11-18 23:25:38 -0800

    Add files via upload

--- hotelreview/hotelreviewapp/urls.py
+++ hotelreview/hotelreviewapp/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from hotelreviewapp.views import HomeView # Import the new view we created
+from hotelreviewapp.views import HotelView
 from . import views
 
-urlpatterns = [
+urlpatterns =[
     path('admin/', admin.site.urls),
     path('', HomeView.as_view()), # Map the HomeView to the index route
+    path('hotel/', HotelView.as_view()), # Map the HomeView to the index route
     #path('index/', views.index, name='index'),
     path('reviewscore/', views.reviewscore, name='reviewscore'),
+    path('similarhotel/', views.similarhotel, name='similarhotel'),
 ]
